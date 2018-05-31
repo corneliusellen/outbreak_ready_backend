eea7d745ef9aecc35cd411774ab9ac4590b42b3e
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-31 17:40:07 -0600

    Implement edge case in controller.

--- app/controllers/api/v1/search/restaurants_controller.rb
+++ app/controllers/api/v1/search/restaurants_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::Search::RestaurantsController < ApplicationController
 
   def index
     restaurants = RestaurantService.new(params[:near], params[:query]).run
-    render json: restaurants, each_serializer: RestaurantSerializer
+    if restaurants.empty?
+      render json: {message: "No restaurants found for your search"}
+    else
+      render json: restaurants, each_serializer: RestaurantSerializer
+    end
   end
 end
