d9f542c910c0d290898f76f33a5c341da76663fb
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-01 18:27:39 -0600

    Create get menu route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :search do
         get '/restaurants', to: 'restaurants#index'
+        get '/menus', to: 'menus#index'
       end
     end
   end
