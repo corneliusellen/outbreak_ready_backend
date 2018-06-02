f0187d19056ee3f544f21665e8081961fde0ae6d
Ellen Cornelius <ecorneli@iwu.edu>
2018-06-02 10:27:02 -0600

    Create route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/tags', to: 'tags#index'
       namespace :search do
         get '/restaurants', to: 'restaurants#index'
         get '/menus', to: 'menus#index'
